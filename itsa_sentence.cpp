324a57cdd336b5d837f1bdf199f4568427273562
QIUXIANG126 <127104134+QIUXIANG126@users.noreply.github.com>
2023-05-03 21:18:29 +0800

    Update itsa_sentence.cpp

--- itsa_sentence.cpp
+++ itsa_sentence.cpp
@@ -1,41 +1,41 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
 int main(){
-    string a;
-    string KEY="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    size_t found;
-    getline(cin,a);
-    if(a.size()>1000)a.resize(1000);
-    for(int i=0;i<26;i++){
-        found =-1;
-        do{
-            found=a.find(KEY[i],found+1);
-            if(found!=string::npos) a[found]+=32;
-        }while(found!=string::npos);
-    }
-    found=0;
-    size_t found2,found3;string b;
-    int c,g=-1;
-    while(found!=string::npos){
-        found2=found;
-        found=a.find(" ",found+1);
-        if(found2==0){
-            b=a.substr(found2,found);
-        }else{
-            b=a.substr(found2+1,found-found2);
+    string In,Out1,Out2;
+    getline(cin,In);
+    if(In.size()<=1000){
+        for(int i=0;i<In.size();i++){
+        if(In[i]>=65 && In[i]<=90)In[i]+=32;
         }
-        if(b[b.size()-1]==' ')b.erase(b.find(" "));
-        found3=a.find(b);
-        c=0;
-        while(found3!=string::npos){
-            if(found3!=string::npos)c+=1;
-            if(c!=1)g=found3;
-            found3=a.find(b,found3+1);
+        In+=" ";
+        size_t found=0;
+        size_t foundRE=0;
+        while(found!=string::npos){
+            int foundL = found;
+            found=In.find(" ",found+1);
+            int foundR = found;
+            for(int i=foundL;i<=foundR;i++){
+                if(In[i]!=' ')Out1+=In[i];
+            }
+
+            if(Out1.size()==1){
+                if(Out1[0]>=97 && Out1[0]<=122){
+                    Out1[0]-=32;
+                }
+            }
+        
+            foundRE=Out2.find(Out1);
+            if(foundRE == string::npos ){
+                Out2+=Out1+" ";
+            }
+            Out1.clear();
         }
-        cout<<g<<endl;
-        if(g!=-1)a.erase(g-1,b.size()+1);
-        g=-1;
+        for(int i=0;i<Out2.size();i++){
+            if(Out2[i]>=65 && Out2[i]<=90)Out2[i]+=32;
+        }
+        Out2.erase(Out2.find_last_of(" "));
+        cout << Out2 << endl;
     }
-    cout<<a;
 }
